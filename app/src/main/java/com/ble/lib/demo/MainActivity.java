13be4290762c834f3215cfa60cfc8a15567a6191
hackill <hankuo@bong.cn>
2017-03-31 17:34:13 +0800

    add guide

--- app/src/main/java/com/ble/lib/demo/MainActivity.java
+++ app/src/main/java/com/ble/lib/demo/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,11 +16,24 @@ import android.widget.Toast;
 import com.ble.lib.BLEInitCallback;
 import com.ble.lib.BleManager;
 import com.ble.lib.GattState;
+import com.ble.lib.x.BongHexUtils;
+import com.ble.lib.x.BongUtil;
 import com.ble.lib.x.XBleManager;
+import com.ble.lib.x.request.XPerReadRequest;
+import com.ble.lib.x.request.XPerReadResponse;
+import com.ble.lib.x.request.XReadRequest;
+import com.ble.lib.x.request.XReadResponse;
+import com.ble.lib.x.request.XResponse;
+import com.ble.lib.x.request.XWriteRequest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private static final String TAG = "MainActivity";
 
     private String mCurMac = "E4:FE:C6:1A:11:48";
     private TextView mMacTextView, mStatusTextView;
@@ -38,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.btn_select).setOnClickListener(this);
         findViewById(R.id.btn_connect).setOnClickListener(this);
         findViewById(R.id.btn_disconnect).setOnClickListener(this);
+        findViewById(R.id.btn_cmd).setOnClickListener(this);
 
         LocalBroadcastManager.getInstance(this).registerReceiver(mBleStateReceiver, new IntentFilter(GattState.BLE_STATE_CHANGE));
 
@@ -65,6 +80,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } else {
                     showToast("先选择设备");
                 }
+                break;
+
+            case R.id.btn_cmd:
+//                cmdDemo();
                 break;
         }
     }
@@ -127,6 +146,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mBleManager.cancelAll();
             mBleManager.disconnect();
         }
+    }
+
+    /**
+     * 三种命令
+     * a、只写指令 没有返回
+     * b、写指令+ 一条返回
+     * c、写指令 + 多条返回
+     */
+    private void cmdDemo() {
+
+        if (mBleManager == null) return;
+
+        //a 类型例子  发送一个震动命令 震动命令是 向设备发送 十六进制指令 260000002001
+        //这类指令 只发送指令，不接受结果
+        byte[] cmdVibrate = BongUtil.hexStringToBytes("260000002001");
+
+        mBleManager.addRequest(new XWriteRequest(cmdVibrate, new XResponse() {
+            @Override
+            public void onError(Exception e) {
+                //发送失败
+            }
+
+            @Override
+            public void onCommandSuccess() {
+                //发送指令成功
+            }
+        }));
+
+        // b 类型例子
+        //类型二 案例：读取电量 . 2600000010是读取电量指令 ，改指令发送后，有一条结果
+        byte[] cmdBattery = BongUtil.hexStringToBytes("2600000010");
+        mBleManager.addRequest(new XPerReadRequest(cmdBattery, new XPerReadResponse() {
+            @Override
+            public void onReceive(byte[] bytes) {
+                //解析回调结果 具体如何解析 请查看指令文档
+                int bu;
+                if (bytes != null && bytes.length > 10) {
+                    bu = (bytes[10] & 0x000000ff);
+                } else {
+                    bu = -1;
+                }
+
+                Log.i(TAG, "电量值为：" + bu);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                //发送失败
+                // 或者发送成功 但是在接收指令返回值超时
+
+            }
+
+            @Override
+            public void onCommandSuccess() {
+
+            }
+        }));
+
+        long endTime = System.currentTimeMillis();
+        long startTime = endTime - TimeUnit.HOURS.toMillis(6);
+        byte[] cmdSync = BongUtil.hexStringToBytes(getSyncDataCmd(startTime, endTime));
+        //类型三  案例：同步最近6小时数据 同步数据 发送指令后，会返回一大堆数据，直到接收到终止符
+        //
+        mBleManager.addRequest(new XReadRequest(cmdSync, new XReadResponse() {
+            @Override
+            public void onReceive(List<byte[]> list) {
+                //总结果 byte list
+            }
+
+            @Override
+            public void onReceivePerFrame(byte[] bytes) {
+                //每条 byte
+
+            }
+
+            @Override
+            public void onError(Exception e) {
+                //出错
+
+            }
+
+            @Override
+            public void onCommandSuccess() {
+
+            }
+        }));
+
+
+    }
+
+
+    private static String getSyncDataCmd(long beginTime, long endTime) {
+        // 这里的规则是使用我们与硬件预定好的时间规则
+        return "2000000013" + BongHexUtils.getStrTimeForHex(beginTime) + BongHexUtils.getStrTimeForHex(endTime);
     }
 
     @Override
